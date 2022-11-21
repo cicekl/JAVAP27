015a4d3fd6015de546e9c6219cff5fd7ce1488a7
Lorena <cicek.lorena@gmail.com>
2022-11-21 13:22:24 +0100

    Vjezba 3

---
+++ SQL/vjezba3.sql
@@ -0,0 +1,13 @@
+drop database if exists hijerarhija;
+create database hijerarhija;
+use hijerarhija;
+
+create table zaposlenik(
+    sifra int not null primary key auto_increment,
+    ime varchar(50),
+    prezime varchar(50),
+    placa decimal(18,2),
+    nadredeni int
+);
+
+alter table zaposlenik add foreign key(nadredeni) references zaposlenik(sifra);
