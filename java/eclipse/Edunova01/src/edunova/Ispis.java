e32205dc2739c546f68fe3d7e78395f9ddb3fdac
Lorena <cicek.lorena@gmail.com>
2022-12-15 13:24:58 +0100

    Java vjezbe

---
+++ java/eclipse/Edunova01/src/edunova/Ispis.java
@@ -0,0 +1,14 @@
+package edunova;
+
+public class Ispis {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Prva linija");
+		System.out.println("Druga linija");
+		System.out.print("Treca linija\n");
+		System.out.print("Cetvrta linija");
+		
+	}
+
+}
