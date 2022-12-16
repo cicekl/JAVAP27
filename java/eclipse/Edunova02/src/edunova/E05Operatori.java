86bf2d249eaf0d0410cc0c913722671511892480
Lorena <cicek.lorena@gmail.com>
2022-12-16 02:41:10 +0100

    Operatori

---
+++ java/eclipse/Edunova02/src/edunova/E05Operatori.java
@@ -0,0 +1,27 @@
+package edunova;
+
+public class E05Operatori {
+
+	public static void main(String[] args) {
+
+		int a = 2;
+		int b = 7;
+
+		float r = (float) a / b;
+
+		System.out.println(r);
+
+		// modulo %
+
+		int i = 7 % 2;
+		
+		System.out.println(i);
+		
+		//nadoljepljivanje (concatination)
+		
+		System.out.println(i + " - rezultat modula.");
+		
+		
+	}
+
+}
