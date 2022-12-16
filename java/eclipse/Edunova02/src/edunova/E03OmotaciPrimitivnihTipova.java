a690800acd2c3644231034a5657fd72adcb4a759
Lorena <cicek.lorena@gmail.com>
2022-12-16 02:35:58 +0100

    Omotači primitivnih tipova

---
+++ java/eclipse/Edunova02/src/edunova/E03OmotaciPrimitivnihTipova.java
@@ -0,0 +1,24 @@
+package edunova;
+
+public class E03OmotaciPrimitivnihTipova {
+	
+	public static void main(String[] args) {
+		
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		
+		
+		int i = Integer.parseInt("3");
+		
+		boolean x = Boolean.parseBoolean("true");
+		short s = Short.parseShort("34");
+		byte b = Byte.parseByte("1");
+		long l = Long.parseLong("345");
+		double d = Double.parseDouble("44");
+		float f = Float.parseFloat("14.56");
+		char c = Character.valueOf('L');
+		
+		
+	}
+
+}
