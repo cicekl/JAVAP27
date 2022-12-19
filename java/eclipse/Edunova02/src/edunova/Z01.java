79f06f5e137a8e0ce2eed40b979021596c6db314
Lorena <cicek.lorena@gmail.com>
2022-12-19 01:11:12 +0100

    Zadatci

---
+++ java/eclipse/Edunova02/src/edunova/Z01.java
@@ -0,0 +1,24 @@
+package edunova;
+
+public class Z01 {
+	public static void main(String[] args) {
+
+		// deklarirati 4 varijable različitih tipova podataka
+		// dodijeliti im vrijednosti te ispisati vrijednosti
+
+		int broj = 4;
+
+		char znak = 'L';
+		
+		double pi = 3.14;
+		
+		boolean laz = false;
+		
+		System.out.println(broj);
+		System.out.println(znak);
+		System.out.println(pi);
+		System.out.println(laz);
+
+	}
+
+}
---
+++ java/eclipse/Edunova02/src/edunova/Z02.java
@@ -0,0 +1,16 @@
+package edunova;
+
+public class Z02 {
+	
+	public static void main(String[] args) {
+		
+		//koristeći Integer omotač interpretirati broj -262 u varijablu negativniBroj
+		//ispisati tu vrijednost
+		
+		int negativniBroj=Integer.parseInt("-262");
+		
+		System.out.println(negativniBroj);
+		
+	}
+
+}
