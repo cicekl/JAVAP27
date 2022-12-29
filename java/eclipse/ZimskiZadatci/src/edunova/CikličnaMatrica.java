8968bc9cfd506ceba92139bb8714347b8c6630cb
Lorena <cicek.lorena@gmail.com>
2022-12-29 20:23:25 +0100

    Početak ciklične matrice

---
+++ java/eclipse/ZimskiZadatci/src/edunova/CikličnaMatrica.java
@@ -0,0 +1,17 @@
+package edunova;
+
+import javax.swing.JOptionPane;
+
+public class CikličnaMatrica {
+
+	public static void main(String[] args) {
+		
+		int n = Integer.parseInt(JOptionPane.showInputDialog("Unesi jedan broj"));
+		
+		int matrica[][] = new int[n][n];
+		
+		
+		
+	}
+	
+}
