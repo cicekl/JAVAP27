be56b58727ce4e67ad97aec494444886ec771545
Lorena <cicek.lorena@gmail.com>
2023-01-02 22:18:30 +0100

    Početak

---
+++ java/eclipse/ZimskiZadatci/src/edunova/E_4.java
@@ -0,0 +1,18 @@
+package edunova;
+
+public class E_4 {
+
+	public static void main(String[] args) {
+		
+	//2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.  
+	//What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+		
+		
+		
+	
+		
+		
+
+	}
+
+}
