430b7cdf66deb42512992f451108a6598335d78f
Lorena <cicek.lorena@gmail.com>
2023-01-10 20:47:00 +0100

    Do while petlja

---
+++ java/eclipse/Edunova04/src/edunova/E02_DoWhilePetlja.java
@@ -0,0 +1,20 @@
+package edunova;
+
+public class E02_DoWhilePetlja {
+
+	public static void main(String[] args) {
+		// za razliku od fora i whilea, do while petlja se barem jednom izvrši
+
+		boolean uvjet = false;
+		do {
+			System.out.println("Osijek.");
+
+	}while(uvjet);
+
+	int i=0;
+	do {
+		System.out.println(++i);
+	}while(i<10);
+		
+}
+}
