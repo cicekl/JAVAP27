61082654d2d5b6a6f26046185619f2df5a42d344
Lorena <cicek.lorena@gmail.com>
2023-01-16 13:26:05 +0100

    Korištenje metoda

---
+++ java/eclipse/Edunova05/src/edunova/E02_KoristenjeMetoda.java
@@ -0,0 +1,24 @@
+package edunova;
+
+public class E02_KoristenjeMetoda {
+
+	public static void main(String[] args) {
+		
+		E01_VrsteMetoda.m1_pozdravi(); //ctrl + klik na metodu
+		
+		System.out.println();
+		
+//		int i = E01_VrsteMetoda.m2_slucajniBroj();
+//		
+//		System.out.println(i);
+		
+		System.out.println(E01_VrsteMetoda.m2_slucajniBroj());
+		
+		E01_VrsteMetoda.m3_zbroji(2, 4, 5);
+		
+		System.out.println(E01_VrsteMetoda.m4_prim(0));
+		
+	}
+	
+	
+}
