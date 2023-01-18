70ec46f1f068c6467d6c8d24111ec8e969670d39
Lorena <cicek.lorena@gmail.com>
2023-01-18 22:06:23 +0100

    Nadopuna

--- java/eclipse/Edunova06/src/edunova/Pomocno.java
+++ java/eclipse/Edunova06/src/edunova/Pomocno.java
@@ -30,6 +30,38 @@ public class Pomocno {
 			return "";
 		}
 		
+		public static int ucitajCijeliBrojURasponu(String poruka, int odBroj, int doBroja) {
+
+			int i = ucitajCijeliBroj(poruka);
+			while (true) {
+				if (i >= odBroj && i <= doBroja) {
+					return i;
+				} else {
+					JOptionPane.showMessageDialog(null, "Broj mora biti veći od 0 i manji od 6");
+					i = ucitajCijeliBroj(poruka);
+				}
+
+			}
+
+		}
 		
-	}
+		public static int ucitajCijeliBroj(String string) {
+			return ucitajCijeliBroj(string, "Greška kod unosa broja");
+		}
+
+		
+		public static int ucitajCijeliPozitivniBroj(String poruka) {
+			int i = ucitajCijeliBroj(poruka);
+			while (true) {
+				if (i > 0) {
+					return i;
+				} else {
+					JOptionPane.showMessageDialog(null, "Broj mora biti veći od 0");
+					i = ucitajCijeliBroj(poruka);
+				}
+			}
+		}
+		
+		
+}
 
