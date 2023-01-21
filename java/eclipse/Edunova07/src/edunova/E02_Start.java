9ecef1b6e39aee2dfa58a9035b4ad5a5161895e0
Lorena <cicek.lorena@gmail.com>
2023-01-21 21:52:06 +0100

    Objekt

---
+++ java/eclipse/Edunova07/src/edunova/E02_Start.java
@@ -0,0 +1,12 @@
+package edunova;
+
+public class E02_Start {
+	
+	//objekt je instanca klase!!
+	//Osoba je tip podatka - klasa
+	//osoba je instanca klase/objekt/varijabla
+	//new: ključna riječ za poziv konstruktora klase
+	
+	Osoba osoba = new Osoba();
+
+}
