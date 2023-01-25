d5c2a8ef853bdb42364af0b7eb1f127df98d3e1a
Lorena <cicek.lorena@gmail.com>
2023-01-25 13:53:54 +0100

    Ispravak kalkulatora

--- java/eclipse/LjubavniKalkulator/src/edunova/LjubavniKalkulator.java
+++ java/eclipse/LjubavniKalkulator/src/edunova/LjubavniKalkulator.java
@@ -4,25 +4,29 @@ public class LjubavniKalkulator {
 	
 	public static void main(String[] args) {
 		
-		String prvoIme = Metode.unosImena("Unesi prvo ime:").toUpperCase();
-		String drugoIme = Metode.unosImena("Unesi drugo ime:").toUpperCase();
-		int brojac=0;
-		String imena = prvoIme+drugoIme;
-		for(int i=0;i<imena.length();i++) {
-			brojac++;
-		}
+		String prvoIme = Metode.unosImena("Unesi prvo ime:");
+		String drugoIme = Metode.unosImena("Unesi drugo ime:");
+		String imena = prvoIme.toUpperCase()+drugoIme.toUpperCase();
+//		for(int i=0;i<imena.length();i++) {
+//			brojac++;
+//		}
+		int brojac=imena.length();
 		
 		int prviRed [] = new int[brojac];
 		
 		int i, index=0;
 		
-		for(i=0;i<brojac;i++) {
+		for(i=0;i<imena.length();i++) {
 			prviRed[i]=Metode.brojacSlova(imena, i);
+//			 char c = imena.charAt(i);
+//			    if (imena.indexOf(c) == imena.lastIndexOf(c)) {
+//			        prviRed[index] = Metode.brojacSlova(imena, c);
+//			        index++;
+//			    }
 		}
 		
-		int postotakKalkulatora [] = Metode.zbrajanjeBrojeva(brojac, prviRed);
-		System.out.println(prvoIme+" i "+drugoIme+" se vole "+postotakKalkulatora[0]+postotakKalkulatora[1]+"%.");
-		
+//		int broj=Metode.zbrajanjeBrojeva(brojac, prviRed);
+//		System.out.println(broj);
 		
 	}
 
