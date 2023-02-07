3e03d4edbc1aff1664f9048360398d8b8ab914b5
Lorena <cicek.lorena@gmail.com>
2023-02-07 21:40:44 +0100

    Klasa Polaznik

---
+++ java/eclipse/Edunova08/src/edunova/Polaznik.java
@@ -0,0 +1,43 @@
+package edunova;
+
+public class Polaznik extends Osoba  {
+	
+private String brojUgovora;
+	
+	// konstruktor, poziva se s ključnom riječi new
+	public Polaznik() {
+		//super(); // poziv konstruktora iz klase Osoba
+		System.out.println("Konstruktor Polaznik");
+	}
+	
+	public Polaznik(String ime, String prezime, String brojUgovora) {
+		super(ime, prezime);
+		this.brojUgovora = brojUgovora;
+	}
+
+
+	public String getBrojUgovora() {
+		/*protected u nadklasi*/
+		status=1;
+		/*package u nadklasi*/
+		broj=2;
+		
+		return brojUgovora;
+	}
+
+	public void setBrojUgovora(String brojUgovora) {
+		// this je ova klasa u kojoj se nalazim
+		this.brojUgovora = brojUgovora; // bez this je varijabla u ovoj metodi u kojoj se nalazim
+		
+		super.brojUgovora="fiksno";
+		// broj je varijabla u nad klasi
+		broj=3;
+		
+	}
+	
+	@Override
+	public String pozdravi() {
+		return "Polazniče " + super.pozdravi();
+	}
+
+}
