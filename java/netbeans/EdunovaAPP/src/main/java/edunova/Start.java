d5ab8e554fc810f0cbca9f111ac7965e4173632d
Lorena <cicek.lorena@gmail.com>
2023-02-21 20:33:44 +0100

    ORM uvod

---
+++ java/netbeans/EdunovaAPP/src/main/java/edunova/Start.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edunova;
+
+/**
+ *
+ * @author Lorena
+ */
+public class Start {
+
+    public static void main(String[] args) {
+        
+    }
+}
--- java/netbeans/HelloWorld/src/main/java/edunova/HelloWorld.java
+++ java/netbeans/HelloWorld/src/main/java/edunova/HelloWorld.java
@@ -12,5 +12,17 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("");
+        
+        Osoba osoba = new Osoba();
+        
+        osoba.setIme("Pero");
+        osoba.setPrezime("Perić");
+        
+        System.out.println(osoba.getIme());
+        
+        new Prozor().setVisible(true);
     }
+    
+    
 }
